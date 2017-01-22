8d241d80d0f3b8bf13785be64742137ce7ba5d47
Daniel <DanielKolz@users.noreply.github.com>
2017-01-22 18:55:50 +0100

    uploading initial index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+
+require("jasra.php");
+
+$api = new Jasra();
+
+
+$api -> GET("/persons", function ($data, $api) {
+       //do something
+});
+
+$api -> GET("/persons/:id", function ($data, $api) {
+       //do something
+});
+
+$api -> POST("/persons/:id", function ($data, $api) {
+       //do something
+});
+
+$api -> DELETE("/persons/:id", function ($data, $api) {
+       //do something
+});
+
+$api -> PUT("/persons/:id/name", function ($data, $api) {
+       //do something
+});
+
+
+$api -> handleRequest();
+
+?>
